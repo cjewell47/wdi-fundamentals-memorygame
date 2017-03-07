0a09aec3d1f96aeee65066dc1805d8e5c54eeafe
Charles Jewell <charlesjewell@Charless-MacBook-Air.local>
2017-03-07 17:45:00 +0000

    crude alerts

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,6 +1,24 @@
-console.log("JS file is connected to HTML! Woo!")
+console.log("JS file is connected to HTML! Woo!");
 
 var cardOne = "king";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "queen";
+
+if (cardOne === cardTwo) {
+	alert("You have found a match!");
+} else { 
+	alert("Sorry, try again.");
+}
+
+if (cardThree === cardFour) {
+	alert("You have found a match!");
+} else { 
+	alert("Sorry, try again.");
+}
+
+if (cardOne === cardThree) {
+	alert("You have found a match!");
+} else { 
+	alert("Sorry, try again.");
+}

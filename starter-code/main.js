eb8927d91bce201088d276bb097f4552dbc1e2a4
Charles Jewell <charles.jewell47@gmail.com>
2017-03-10 17:39:37 +0000

    final

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,9 +1,12 @@
 console.log("JS file is connected to HTML! Woo!");
 
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
+//var cardOne = "king";
+//var cardTwo = "king";
+//var cardThree = "queen";
+//var cardFour = "queen";
+ 
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
 /* if (cardOne === cardTwo) {
 	alert("You have found a match!");
@@ -23,15 +26,44 @@ if (cardOne === cardThree) {
 	alert("Sorry, try again.");
 } */
 
-var gameBoard = document.querySelector("#game-board");
+var gameBoard = document.getElementById("game-board");
 
 var createCards = function() {
 	for (var i = 0; i < 4; i++){
-
 		var cardElement = document.createElement("div");
 		cardElement.className = "card";
 		gameBoard.appendChild(cardElement);
 	};
 };
 
-document.addEventListener("load", createCards());
+createCards();
+
+var createBoard = function() {
+	for (var x = 0; x < cards.length; x++){
+ 	cardElement.setAttribute('data-card', cards[x]);
+	};
+};
+
+createBoard();
+
+var isMatch = function(match) {
+(match[0].getAttribute('data-card') === match[1].getAttribute('data-card'));
+};
+
+var isTwoCards = function() {
+cardsInPlay.push(this.getAttribute('data-card'));
+if (cardsInPlay.length === 2) {
+	isMatch(cardsInPlay);
+	cardsInPlay = [];
+};
+};
+
+for (var z = 0; z < cards.length; z++) {
+	cardElement.addEventListener("click", isTwoCards());
+	if (cardsInPlay.getAttribute('data-card') === 'king') {
+		cardElement.innerHTML = '<img src="file:///Users/charlesjewell/Documents/fundamentals/wdi-fundamentals-memorygame/starter-code/MyKing.png" alt="King of Spades" />';
+	} else if (cardsInPlay.getAttribute('data-card') === 'queen') {
+		cardElement.innerHTML = '<img src="file:///Users/charlesjewell/Documents/fundamentals/wdi-fundamentals-memorygame/starter-code/MyQueen.png" alt="Queen of Spades" />';
+	};
+};
+
